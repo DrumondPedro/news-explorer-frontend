355d24e03b48fbd2999c1e9b5a3a5ce8f97fed64
pedrumond <pe.drumond97@gmail.com>
2025-04-26 17:53:01 -0300

    feat: implement header responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,7 @@
 import { useContext, useEffect, useState } from 'react';
 
 import { useLocation } from 'react-router-dom';
+import useWindowDimension from 'use-window-dimensions';
 
 import { CurrentUserContext } from '../../contexts/CurrentUserContext';
 import { LoginContext } from '../../contexts/LoginContext';
@@ -9,52 +10,121 @@ import logoWite from '../../assets/images/Header/logo_wite.svg';
 import logoBlack from '../../assets/images/Header/logo_black.svg';
 import loguotWite from '../../assets/images/Header/logout_icon_wite.svg';
 import loguotBlack from '../../assets/images/Header/logout_icon_black.svg';
+import gridIcon from '../../assets/images/Header/menu_grid_icon.svg';
+import gridIconBlack from '../../assets/images/Header/menu_grid_icon_black.svg';
+import closeIcon from '../../assets/images/Header/close_icon.svg';
 
 function Header({ children }) {
   const location = useLocation();
+  const { width } = useWindowDimension();
 
   const { currentUser } = useContext(CurrentUserContext);
   const { isLoggedIn } = useContext(LoginContext);
 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isSavedNewsPage, setIsSavedNewsPage] = useState(false);
 
+  const handleOpeningMenuMobile = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const getLogoImage = () => {
+    if (isMenuOpen) {
+      return logoWite;
+    }
+    if (isSavedNewsPage) {
+      return logoBlack;
+    }
+    return logoWite;
+  };
+
+  const getMenuButtonImage = () => {
+    if (isMenuOpen) {
+      return closeIcon;
+    }
+    if (isSavedNewsPage) {
+      return gridIconBlack;
+    }
+    return gridIcon;
+  };
+
+  const handleHeaderButtonSelection = () => {
+    if (width <= 580) {
+      return (
+        <button
+          onClick={handleOpeningMenuMobile}
+          className='header__menu-button'
+        >
+          <img
+            src={getMenuButtonImage()}
+            alt='Ícone de um x'
+            className='header__menu-button-image'
+          />
+        </button>
+      );
+    }
+    return (
+      <button
+        className={`header__button 
+    ${isSavedNewsPage ? 'header__button--light-theme' : ''} 
+    ${isLoggedIn ? '' : 'header__button--not-loged'}`}
+      >
+        <p
+          className={`header__button-text ${
+            isSavedNewsPage ? 'header__button-text--light-theme' : ''
+          }`}
+        >
+          {isLoggedIn ? `${currentUser.name}` : 'Entrar'}
+        </p>
+        {isLoggedIn && (
+          <img
+            className='header__button-image'
+            src={isSavedNewsPage ? loguotBlack : loguotWite}
+            alt=''
+          />
+        )}
+      </button>
+    );
+  };
+
   useEffect(() => {
+    if (width > 580) {
+      setIsMenuOpen(false);
+    }
+
+    setIsMenuOpen(false);
     setIsSavedNewsPage(location.pathname === '/saved-news');
-  }, [location.pathname]);
+  }, [location.pathname, width]);
 
   return (
-    <header
-      className={`header ${isSavedNewsPage ? 'header--light-theme' : ''}`}
-    >
-      <div className='header__content'>
-        <img
-          className='header__logo'
-          src={isSavedNewsPage ? logoBlack : logoWite}
-          alt=''
-        />
-        {children}
-        <button
-          className={`header__button 
-          ${isSavedNewsPage ? 'header__button--light-theme' : ''} 
-          ${isLoggedIn ? '' : 'header__button--not-loged'}`}
-        >
-          <p
-            className={`header__button-text ${
-              isSavedNewsPage ? 'header__button-text--light-theme' : ''
+    <>
+      <header
+        className={`header ${isSavedNewsPage ? 'header--light-theme' : ''}`}
+      >
+        <div className='header__content'>
+          <img className='header__logo' src={getLogoImage()} alt='' />
+          {width > 580 && children}
+          {handleHeaderButtonSelection()}
+        </div>
+      </header>
+      {isMenuOpen && (
+        <div className='header-menu-popup'>
+          {children}
+          <button
+            className={`header__button ${
+              isLoggedIn ? '' : 'header__button--not-loged'
             }`}
           >
-            {isLoggedIn ? `${currentUser.name}` : 'Entrar'}
-          </p>
-          {isLoggedIn && (
-            <img
-              className='header__button-image'
-              src={isSavedNewsPage ? loguotBlack : loguotWite}
-              alt=''
-            />
-          )}
-        </button>
-      </div>
-    </header>
+            <p className={`header__button-text`}>
+              {isLoggedIn ? `${currentUser.name}` : 'Entrar'}
+            </p>
+            {isLoggedIn && (
+              <img className='header__button-image' src={loguotWite} alt='' />
+            )}
+          </button>
+        </div>
+      )}
+    </>
   );
 }
 
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,23 +1,31 @@
 import { useContext, useEffect, useState } from 'react';
 
 import { NavLink, useLocation } from 'react-router-dom';
+import useWindowDimension from 'use-window-dimensions';
 
 import { LoginContext } from '../../contexts/LoginContext';
 
 function Navigation() {
   const location = useLocation();
+  const { width } = useWindowDimension();
 
   const { isLoggedIn } = useContext(LoginContext);
 
   const [isSavedNewsPage, setIsSavedNewsPage] = useState(false);
 
-  const customClassName = ({ isActive }) =>
-    'navigation__link' +
-    (isActive ? ' navigation__link--active' : '') +
-    (isActive && isSavedNewsPage
-      ? ' navigation__link--active--light-theme'
-      : '') +
-    (isSavedNewsPage ? ' navigation__link--light-theme' : '');
+  const customClassName = ({ isActive }) => {
+    if (width <= 580) {
+      return 'navigation__link';
+    }
+    return (
+      'navigation__link' +
+      (isActive ? ' navigation__link--active' : '') +
+      (isActive && isSavedNewsPage
+        ? ' navigation__link--active--light-theme'
+        : '') +
+      (isSavedNewsPage ? ' navigation__link--light-theme' : '')
+    );
+  };
 
   useEffect(() => {
     setIsSavedNewsPage(location.pathname === '/saved-news');

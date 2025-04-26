d5c91127d6af94249c9c2d5ed54e3ab48fdead8a
pedrumond <pe.drumond97@gmail.com>
2025-04-26 19:46:13 -0300

    feat: create Main component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,10 @@
 import Header from '../Header/Header';
 import Navigation from '../Navigation/Navigation';
+import Main from '../Main/Main';
+
 import Footer from '../Footer/Footer';
+
+import { Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -9,8 +13,23 @@ function App() {
         <Navigation />
       </Header>
       <div className='page'>
-        <Footer />
+        <Routes>
+          <Route path='/' element={<Main />}></Route>
+          <Route
+            path='/saved-news'
+            element={
+              <div
+                style={{
+                  width: '100%',
+                  backgroundColor: '#02370080',
+                  height: '500px',
+                }}
+              ></div>
+            }
+          ></Route>
+        </Routes>
       </div>
+      <Footer />
     </>
   );
 }
---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,15 @@
+function Main() {
+  return (
+    <main className='main'>
+      <div className='main__content'>
+        <h1 className='main__title'>O que está acontecendo no mundo?</h1>
+        <p className='main__text'>
+          Encontre as últimas notícias sobre qualquer tema e salve elas em sua
+          conta pessoal
+        </p>
+      </div>
+    </main>
+  );
+}
+
+export default Main;

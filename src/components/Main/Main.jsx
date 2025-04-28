f039c2171b934cb94409e88139c5865283a28136
pedrumond <pe.drumond97@gmail.com>
2025-04-28 17:24:39 -0300

    feat: create NewsCardList component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,10 +1,12 @@
 import Search from './components/Search/Search';
+import NewsCardList from './components/NewsCardList/NewsCardList';
 import About from './components/About/About';
 
 function Main() {
   return (
     <main className='content'>
       <Search />
+      <NewsCardList />
       <About />
     </main>
   );
---
+++ src/components/Main/components/NewsCardList/NewsCardList.jsx
@@ -0,0 +1,13 @@
+function NewsCardList() {
+  return (
+    <section className='news-card-list'>
+      <h2 className='news-card-list__title'>Procurar resultados</h2>
+      <div className='news-card-list__content'>
+        <div className='news-card-list__cards-area'></div>
+        <button className='news-card-list__button'>Mostrar mais</button>
+      </div>
+    </section>
+  );
+}
+
+export default NewsCardList;

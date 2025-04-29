534297f94ab848071ca6225a16903ebe39e4a6f7
pedrumond <pe.drumond97@gmail.com>
2025-04-29 14:53:03 -0300

    feat: implement NewsCardList  responsiveness

--- src/components/Main/components/NewsCardList/NewsCardList.jsx
+++ src/components/Main/components/NewsCardList/NewsCardList.jsx
@@ -9,14 +9,16 @@ function NewsCardList() {
 
   return (
     <section className='news-card-list'>
-      <h2 className='news-card-list__title'>Procurar resultados</h2>
       <div className='news-card-list__content'>
-        <ul className='news-card-list__cards-area'>
-          {newsData.map((news, i) => (
-            <NewsCard key={i} news={news}></NewsCard>
-          ))}
-        </ul>
-        <button className='news-card-list__button'>Mostrar mais</button>
+        <h2 className='news-card-list__title'>Procurar resultados</h2>
+        <div className='news-card-list__news'>
+          <ul className='news-card-list__cards-area'>
+            {newsData.map((news, i) => (
+              <NewsCard key={i} news={news}></NewsCard>
+            ))}
+          </ul>
+          <button className='news-card-list__button'>Mostrar mais</button>
+        </div>
       </div>
     </section>
   );

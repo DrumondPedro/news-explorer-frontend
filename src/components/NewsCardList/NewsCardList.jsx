05df96f72bbf6f480d02ba48e3ca5d01395b1957
pedrumond <pe.drumond97@gmail.com>
2025-04-30 17:03:25 -0300

    feat: update NewsCardList component

--- src/components/NewsCardList/NewsCardList.jsx
+++ src/components/NewsCardList/NewsCardList.jsx
@@ -3,21 +3,27 @@ import { useContext } from 'react';
 import NewsCard from '../NewsCard/NewsCard';
 
 import { NewsContext } from '../../contexts/NewsContext';
+import { LocationContext } from '../../contexts/LocationContext';
 
 function NewsCardList() {
   const { newsData } = useContext(NewsContext);
+  const { isSavedNewsPage } = useContext(LocationContext);
 
   return (
     <section className='news-card-list'>
       <div className='news-card-list__content'>
-        <h2 className='news-card-list__title'>Procurar resultados</h2>
+        {!isSavedNewsPage && (
+          <h2 className='news-card-list__title'>Procurar resultados</h2>
+        )}
         <div className='news-card-list__news'>
           <ul className='news-card-list__cards-area'>
             {newsData.map((news, i) => (
               <NewsCard key={i} news={news}></NewsCard>
             ))}
           </ul>
-          <button className='news-card-list__button'>Mostrar mais</button>
+          {!isSavedNewsPage && (
+            <button className='news-card-list__button'>Mostrar mais</button>
+          )}
         </div>
       </div>
     </section>

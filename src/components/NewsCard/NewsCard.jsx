530440f688e5e3cd6b27a40b8bb4eddc3990ffd0
pedrumond <pe.drumond97@gmail.com>
2025-05-06 20:51:23 -0300

    feat: update NwesCard component

--- src/components/NewsCard/NewsCard.jsx
+++ src/components/NewsCard/NewsCard.jsx
@@ -1,16 +1,38 @@
-import { useEffect, useState } from 'react';
+import { useContext, useEffect, useState } from 'react';
+
+import { LocationContext } from '../../contexts/LocationContext';
 
 import SaveButton from './components/SaveButton/SaveButton';
 
 function NewsCard({ news }) {
+  const { isSavedNewsPage } = useContext(LocationContext);
+
   const [currentNews, setCurrentNews] = useState(news);
+  const [isHover, setIsHover] = useState(false);
+
+  const handleHoverCard = () => {
+    if (isHover === false) {
+      setIsHover(true);
+      return;
+    }
+    setIsHover(false);
+  };
 
   useEffect(() => {
     setCurrentNews(news);
   }, [news]);
 
   return (
-    <li className='news-card'>
+    <li
+      className='news-card'
+      onMouseEnter={handleHoverCard}
+      onMouseLeave={handleHoverCard}
+    >
+      {isSavedNewsPage && !isHover && (
+        <div className='news-card__topic-popup'>
+          <p className='news-card__topic-popup-text'>{currentNews.topic}</p>
+        </div>
+      )}
       <SaveButton />
       <img className='news-card__image' src={currentNews.image} alt='' />
       <div className='news-card__content'>

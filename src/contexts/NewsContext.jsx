a1426bd60db9d04abe7457b6e79df3c9317fc61e
pedrumond <pe.drumond97@gmail.com>
2025-05-08 20:59:40 -0300

    feat: create new function in NewsContext

--- src/contexts/NewsContext.jsx
+++ src/contexts/NewsContext.jsx
@@ -1,16 +1,36 @@
 import { createContext, useState } from 'react';
 
-import { news } from '../../cards-test';
+import { newsClient } from '../utils/NewsApi';
 
 export const NewsContext = createContext();
 
 export function NewsProvider({ children }) {
-  const [newsData, setNewsData] = useState(news);
+  const [newsData, setNewsData] = useState([]);
+
+  async function handleNewsResearch(keywords) {
+    const today = new Date();
+    const lastWeek = new Date(today - 604800000);
+    const todayISOS = today.toISOString().substring(0, 10);
+    const lastWeekISOS = lastWeek.toISOString().substring(0, 10);
+
+    try {
+      const data = await newsClient.getNews({
+        keywords,
+        lastWeek: lastWeekISOS,
+        today: todayISOS,
+      });
+      setNewsData(data.articles);
+    } catch (error) {
+      console.error('GET - /newsapi -', error);
+      setNewsData([]);
+    }
+  }
 
   return (
     <NewsContext.Provider
       value={{
         newsData,
+        handleNewsResearch,
       }}
     >
       {children}
